7463ad29de00b72392c4c26836814e7d9ae05086
kdragonkorea <morenmore6340@gmail.com>
2021-07-19 10:17:42 +0900

    SQL 문법

--- SQL 문법.sql
+++ SQL 문법.sql
@@ -11,3 +11,43 @@ FROM CLASSICMODELS.PAYMENTS;
 SELECT PRODUCTNAME, PRODUCTLINE
 FROM CLASSICMODELS.PRODUCTS;
 
+-- AS
+SELECT COUNT(PRODUCTCODE)
+AS n_products
+FROM CLASSICMODELS.PRODUCTS;
+
+-- DISTINCT
+SELECT DISTINCT ORDERNUMBER
+FROM classicmodels.orderdetails;
+
+-- BETWEEN (WHERE)
+select *
+from classicmodels.orderdetails
+where priceeach between 30 and 50;
+
+-- =/>/</>=/<=/<>(같지 않다)/ 
+select *
+from classicmodels.orderdetails
+where priceeach >= 30;
+
+-- IN, Not In
+select customernumber
+from classicmodels.customers
+where country in ('USA', 'CANADA');
+
+select customernumber
+from classicmodels.customers
+where country not in ('usa','canada');
+
+-- IS NULL, IS NOT NULL
+select employeenumber
+from classicmodels.employees
+where reportsTo is null;
+
+ -- LIKE '%TEXT%'
+ select addressline1
+ from classicmodels.customers
+ where addressline1 like '%ST%';
+ 
+ -- GROUP BY
+ -- test 
